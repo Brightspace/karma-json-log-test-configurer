a92e58979b698aab3cd01786a40d6d0b5d906910
Noel Stonehouse <noel.stonehouse@D2L.com>
2014-10-22 14:35:46 -0400

    Remove all classes from element up through parents for diffing. Dump JSON in nicer format. Update coverage reporter.

--- differs.js
+++ differs.js
@@ -2,37 +2,58 @@ var d2l = d2l || {};
 d2l.vui = d2l.vui || {};
 
 d2l.vui.differs = {
+	_private: {
+		createDeclassedClone: function( classedElement ) {
+			if( classedElement == document.body ) {
+				return classedElement;
+			}
+
+			var element = classedElement.cloneNode(false);
+			element.className="";
+			var parent = d2l.vui.differs._private.createDeclassedClone( classedElement.parentNode )
+				.appendChild( element );
+
+			return element;
+		},
+
+		removeDeclassedClone: function( declassedElement ) {
+			if( !declassedElement.parent ) {
+				return;
+			}
+
+			declassedElement.parent.removeChild( declassedElement );
+			removeDeclassedClone( declassedElement.parent );
+		}
+	},
 
 	diffDefaultStyle: function( classStyledElement, pseudoElt ) {
-		var defaultElement = classStyledElement.cloneNode(true);
-		defaultElement.className="";
-		classStyledElement.parentNode.appendChild( defaultElement );
+		var defaultElement = d2l.vui.differs._private.createDeclassedClone(classStyledElement);
 
-    	var actualComputed = window.getComputedStyle( classStyledElement, pseudoElt || null );
+		var actualComputed = window.getComputedStyle( classStyledElement, pseudoElt || null );
 
 		var defaultComputed = window.getComputedStyle( defaultElement, pseudoElt || null );
 
-        if (!actualComputed || !defaultComputed) {
-			classStyledElement.parentNode.removeChild( defaultElement );
-            return null;
-        }
+		if (!actualComputed || !defaultComputed) {
+			d2l.vui.differs._private.removeDeclassedClone( defaultElement );
+			return null;
+		}
 
-        var diff = {};
+		var diff = {};
 
-        for (var i = 0; i < actualComputed.length; i++) {
-        	var aName = actualComputed.item(i);
+		for (var i = 0; i < actualComputed.length; i++) {
+			var aName = actualComputed.item(i);
 
-            var actualValue = actualComputed.getPropertyValue(aName);
-            var defaultValue = defaultComputed.getPropertyValue(aName);
+			var actualValue = actualComputed.getPropertyValue(aName);
+			var defaultValue = defaultComputed.getPropertyValue(aName);
 
-            if ( actualValue === defaultValue ) {
-                continue;
-            }
+			if ( actualValue === defaultValue ) {
+				continue;
+			}
 
-            diff[aName] = actualValue;
-        }
+			diff[aName] = actualValue;
+		}
 
-		classStyledElement.parentNode.removeChild( defaultElement );
+		d2l.vui.differs._private.removeDeclassedClone( defaultElement );
 
 		return diff;
 	}
